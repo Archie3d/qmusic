367f986980e2c15f5619a76ca8a7b4c726e41ae0
Arthur Benilov <arthur.benilov@gmail.com>
2015-08-12 11:51:37 +0200

    Add simple wave files reader.

---
+++ source/framework/include/WavFile.h
@@ -0,0 +1,136 @@
+#ifndef WAVFILE_H
+#define WAVFILE_H
+
+#include <QFile>
+#include "FrameworkApi.h"
+
+/**
+ * @brief Wav files reader.
+ *
+ * This class supports only PCM encoded files.
+ */
+class QMUSIC_FRAMEWORK_API WavFile
+{
+public:
+
+    /**
+     * Wav file chunk IDs.
+     */
+    enum Chunk {
+        Chunk_RiffHeader = 0x46464952,
+        Chunk_WavRiff = 0x54651475,
+        Chunk_Format = 0x20746D66,
+        Chunk_LabeledText = 0x478747C6,
+        Chunk_Instrumentation = 0x478747C6,
+        Chunk_Sample = 0x6C706D73,
+        Chunk_Fact = 0x47361666,
+        Chunk_Data = 0x61746164,
+        Chunk_Junk = 0x4B4E554A
+    };
+
+    /**
+     * Data encoding format.
+     */
+    enum Format {
+        Format_PCM = 0x01,
+        Format_FloatingPoint = 0x03,
+        Format_ALaw = 0x06,
+        Format_MuLaw = 0x07,
+        Format_IMAADPCM = 0x11,
+        Format_YamahaITUG723ADPCM = 0x16,
+        Format_GSM610 = 0x31,
+        Format_ITUG721ADPCM = 0x40,
+        Format_MPEG = 0x50,
+        Format_Extensible = 0xFFFE
+    };
+
+    /**
+     * Initialize the object, but do not open the file.
+     * @param filePath Absolute file path.
+     */
+    WavFile(const QString &filePath);
+
+    /**
+     * Destructor.
+     * This will also close the file if open.
+     */
+    ~WavFile();
+
+    /**
+     * Open the file for reading.
+     * @return true if opened OK.
+     */
+    bool open();
+
+    /**
+     * Close the file.
+     */
+    void close();
+
+    /**
+     * Tells whether the file is open.
+     * @return true if the file is open.
+     */
+    bool isOpen() const;
+
+    /**
+     * Tells whether there was an error detected during
+     * the data readgin and processing
+     * @return true if an error was detected.
+     */
+    bool isError() const { return m_error; }
+
+    /**
+     * Returns the error message.
+     * @note The return is valid only if the error flag is set.
+     * @return Error message.
+     */
+    QString errorText() const { return m_errorText; }
+
+    /**
+     * Read the file header.
+     * This will read the file till the data chunk is encountered.
+     * @return true if header has been read successfully.
+     */
+    bool readHeader();
+
+    /**
+     * Read data of a single channel wave file.
+     * @param data Read data
+     * @return true if read Ok.
+     */
+    bool readSingleChannelData(QVector<float> &data);
+
+    // format accessors
+    Format format() const { return m_format; }
+    int numberOfChannels() const { return m_numberOfChannels; }
+    int sampleRate() const { return m_sampleRate; }
+    int byteRate() const { return m_byteRate; }
+    int blockAlign() const { return m_blockAlign; }
+    int bitsPerSample() const { return m_bitsPerSample; }
+    int numberOfSamples() const { return m_numberOfSamples; }
+
+private:
+
+    void setError(const QString &text);
+    void clearError();
+
+    bool readSingleSample(float &v);
+
+    QString m_filePath;
+    QFile m_file;
+    bool m_error;
+    QString m_errorText;
+
+    // Format data
+    Format m_format;
+    int m_numberOfChannels;
+    int m_sampleRate;
+    int m_byteRate;
+    int m_blockAlign;
+    int m_bitsPerSample;
+    int m_numberOfSamples;
+};
+
+#endif // WAVFILE_H
+
---
+++ source/framework/src/WavFile.cpp
@@ -0,0 +1,216 @@
+#include <QDebug>
+#include "WavFile.h"
+
+#define WAVE_SIGNATURE (0x45564157)
+
+template<typename T>
+bool read(QFile &file, T &v)
+{
+    if (file.isOpen()) {
+        T data;
+        qint64 r = file.read(reinterpret_cast<char*>(&data), sizeof(data));
+        if (r == sizeof(data)) {
+            v = data;
+            return true;
+        } else {
+            qCritical() << "Expected" << sizeof(data) << "read" << r;
+        }
+    }
+    return false;
+}
+
+WavFile::WavFile(const QString &filePath)
+    : m_filePath(filePath)
+{
+    clearError();
+}
+
+WavFile::~WavFile()
+{
+    m_file.close();
+}
+
+bool WavFile::open()
+{
+    clearError();
+    if (m_file.isOpen()) {
+        setError("File is already open");
+        return false;
+    }
+
+    m_file.setFileName(m_filePath);
+    return m_file.open(QIODevice::ReadOnly);
+}
+
+void WavFile::close()
+{
+    m_file.close();
+    clearError();
+}
+
+bool WavFile::isOpen() const
+{
+    return m_file.isOpen();
+}
+
+bool WavFile::readHeader()
+{
+    clearError();
+    if (!isOpen()) {
+        setError("File is not open");
+        return false;
+    }
+
+    // Move to the beginning of the file
+    m_file.seek(0);
+
+    quint32 chunkId = 0;
+    quint32 chunkSize = 0;
+    bool isData = false;
+
+    while (!isData && !isError()) {
+        if (!read<quint32>(m_file, chunkId)) {
+            setError("Unable to read the file chunk ID");
+            break;
+        }
+        if (!read<quint32>(m_file, chunkSize)) {
+            setError("Unable to read chunk size");
+            break;
+        }
+
+        switch (static_cast<Chunk>(chunkId)) {
+        case Chunk_RiffHeader: {
+            quint32 wave = 0;
+            bool ok = read<quint32>(m_file, wave);
+            if (!ok) {
+                setError("Unable to read WAVE signature");
+            } else if (wave != WAVE_SIGNATURE) {
+                setError(QString("WAVE signature not found, but 0x%1 instead")
+                         .arg(QString::number(wave, 16)));
+            }
+            break;
+        }
+        case Chunk_Format: {
+            quint16 fmt;
+            quint16 channels;
+            quint32 sampleRate;
+            quint32 byteRate;
+            quint16 blockAlign;
+            quint16 bitsPerSample;
+
+            bool ok = true;
+            ok &= read<quint16>(m_file, fmt);
+            ok &= read<quint16>(m_file, channels);
+            ok &= read<quint32>(m_file, sampleRate);
+            ok &= read<quint32>(m_file, byteRate);
+            ok &= read<quint16>(m_file, blockAlign);
+            ok &= read<quint16>(m_file, bitsPerSample);
+
+            if (ok) {
+                m_format = static_cast<Format>(fmt);
+                m_numberOfChannels = static_cast<int>(channels);
+                m_sampleRate = static_cast<int>(sampleRate);
+                m_byteRate = static_cast<int>(byteRate);
+                m_blockAlign = static_cast<int>(blockAlign);
+                m_bitsPerSample = static_cast<int>(bitsPerSample);
+            } else {
+                setError("Unable to read format data");
+            }
+            break;
+        }
+        case Chunk_Data:
+            isData = true;
+            m_numberOfSamples = chunkSize / m_blockAlign;
+            break;
+        default:
+            // Skip unknown chunk
+            m_file.seek(m_file.pos() + chunkSize);
+            break;
+        }
+    }
+
+    return !isError();
+}
+
+bool WavFile::readSingleChannelData(QVector<float> &data)
+{
+    clearError();
+    if (!isOpen()) {
+        setError("File is not open");
+        return false;
+    }
+    if (m_format != Format_PCM) {
+        setError("unsupported format (PCM expected)");
+        return false;
+    }
+    if (m_numberOfChannels != 1) {
+        setError(QString("Single channel expected, but %1 found").arg(m_numberOfChannels));
+        return false;
+    }
+
+    bool ok = true;
+    int samples = 0;
+    while (samples < m_numberOfSamples && !m_file.atEnd()) {
+        float v;
+        ok &= readSingleSample(v);
+        if (ok) {
+            data.append(v);
+            samples++;
+        }
+    }
+
+    if (!ok) {
+        setError("Unable to read samples data");
+    }
+
+    return ok;
+}
+
+void WavFile::setError(const QString &text)
+{
+    m_error = true;
+    m_errorText = text;
+}
+
+void WavFile::clearError()
+{
+    m_error = false;
+    m_errorText.clear();
+}
+
+bool WavFile::readSingleSample(float &v)
+{
+    bool ok = true;
+    switch (m_bitsPerSample) {
+    case 8: {
+        quint8 data;
+        ok &= read<quint8>(m_file, data);
+        if (ok) {
+            v = float(data - 127) / 128.0f;
+        }
+        break;
+    }
+    case 16: {
+        qint16 data;
+        ok &= read<qint16>(m_file, data);
+        if (ok) {
+            v = float(data) / 32768.0f;
+        }
+        break;
+    }
+    case 32: {
+        qint32 data;
+        ok &= read<qint32>(m_file, data);
+        if (ok) {
+            v = float(data) / 2147483648.0f;
+        }
+        break;
+    }
+    default:
+        qDebug() << "Unknown bits per sample" << m_bitsPerSample;
+        ok = false;
+        break;
+    }
+
+    return ok;
+}
